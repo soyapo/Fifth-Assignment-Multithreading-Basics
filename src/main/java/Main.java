a4f49d04138dfb39cc2d2664049436211e74d662
AmirAli <amirnasiri1383710@gmail.com>
2025-04-28 17:49:01 +0330

    ReportGenerator Files Added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        try {
-            ReportGenerator.loadProducts();
-
-            ReportGenerator.readFilesWithThreads();
-
-             ReportGenerator.readFilesSingleThreaded();
-
-        } catch (Exception e) {
-            System.err.println("An error occurred: " + e.getMessage());
-        }
-    }
+    
 }
