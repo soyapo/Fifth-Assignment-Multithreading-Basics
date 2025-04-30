39f0d70db8c885697236726941f8fd166e1fd57e
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-30 10:34:05 +0330

    refactor: relocate Product

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -30,6 +30,29 @@ public class ReportGenerator {
         }
     }
 
+    static class Product {
+        private int productID;
+        private String productName;
+        private double price;
+
+        public Product(int productID, String productName, double price) {
+            this.productID = productID;
+            this.productName = productName;
+            this.price = price;
+        }
+
+        public int getProductID() {
+            return productID;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
     private static final String[] ORDER_FILES = {
             //TODO: Add paths to order files
     };
