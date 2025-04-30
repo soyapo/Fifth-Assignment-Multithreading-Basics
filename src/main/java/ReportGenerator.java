5f546a8b39876dba50ec6ad2aa6e8f15ec9b540d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-30 10:24:41 +0330

    refactor: relocate Runnable implementation

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -1,16 +1,41 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class ReportGenerator {
+    static class TaskRunnable implements Runnable {
+        private final String path;
+        private double totalCost;
+        private int totalAmount;
+        private int totalDiscountSum;
+        private int totalLines;
+        private Product mostExpensiveProduct;
+        private double highestCostAfterDiscount;
+
+        public TaskRunnable(String path) {
+            this.path = path;
+            this.totalCost = 0;
+            this.totalAmount = 0;
+            this.totalDiscountSum = 0;
+            this.totalLines = 0;
+            this.highestCostAfterDiscount = 0;
+            this.mostExpensiveProduct = null;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        public void makeReport() {
+
+        }
+    }
 
     private static final String[] ORDER_FILES = {
             //TODO: Add paths to order files
     };
 
     static Product[] productCatalog = new Product[10];
-    static RunnableClass[] tasks = new RunnableClass[4];
+    static TaskRunnable[] tasks = new TaskRunnable[4];
 
     public static void loadProducts() throws IOException {
         //TODO: Read products from Products.txt with a single thread and store them in the productCatalog array
