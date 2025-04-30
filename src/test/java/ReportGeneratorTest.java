348b26c01504897e0e1ec5fd58616be4f9b7b0ce
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-30 16:09:11 +0330

    add ReportGeneratorTest

---
+++ src/test/java/ReportGeneratorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReportGeneratorTest {
+    public static ReportGenerator handle;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+}
